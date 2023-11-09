d57a8da5ce25507044c2671892c7b3456e14ca80
David Heidrich <me@bowlingx.com>
2023-11-09 17:29:03 +0100

    fix: removed unused singleton router

--- src/next-router-shim.ts
+++ src/next-router-shim.ts
@@ -1,9 +1,8 @@
 import { RouterContext } from './context.js'
-import Router from 'next/dist/client/router.js'
 import { useContext } from 'react'
 
 export const useRouter = () => {
   return useContext(RouterContext)
 }
 
-export default Router
+export default {}
