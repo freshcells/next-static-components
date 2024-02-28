0fc0017be8425cb323b51d79bad0539090bdae23
David Heidrich <me@bowlingx.com>
2024-02-28 11:26:56 +0100

    fix: make sure we wait for the dom to be ready

--- src/shell/app-shell.client.tsx
+++ src/shell/app-shell.client.tsx
@@ -14,67 +14,70 @@ declare global {
 }
 
 async function init() {
-  window.dispatchEvent(new CustomEvent(EV_BEFORE_HYDRATION))
+  try {
+    window.dispatchEvent(new CustomEvent(EV_BEFORE_HYDRATION))
 
-  await loadableReady()
+    await loadableReady()
 
-  const initialData = JSON.parse(
-    document.getElementById('__NEXT_STATIC_DATA__')!.textContent!
-  ) as NextStaticData
+    const initialData = JSON.parse(
+      document.getElementById('__NEXT_STATIC_DATA__')!.textContent!
+    ) as NextStaticData
 
-  const thisInitialData = (initialData || {}) as NextStaticData
+    const thisInitialData = (initialData || {}) as NextStaticData
 
-  const config = {
-    ...thisInitialData,
-    context: {
-      ...thisInitialData.context,
-      ...(window.__NEXT_STATIC_CONTEXT_EXTEND__ || {}),
-    },
-  }
+    const config = {
+      ...thisInitialData,
+      context: {
+        ...thisInitialData.context,
+        ...(window.__NEXT_STATIC_CONTEXT_EXTEND__ || {}),
+      },
+    }
 
-  const {
-    locale,
-    locales,
-    basePath,
-    domains,
-    defaultLocale,
-    linkPrefix,
-    context,
-    query,
-  } = config
+    const {
+      locale,
+      locales,
+      basePath,
+      domains,
+      defaultLocale,
+      linkPrefix,
+      context,
+      query,
+    } = config
 
-  const { components, props } = await application(context)
+    const { components, props } = await application(context)
 
-  for (const [index, Component] of components.entries()) {
-    const selector = `[data-next-static-index="${index}"]`
-    const root = document.querySelector(selector)
-    if (!root) {
-      throw new Error(
-        `[next-static] Unable to rehydrate static root. Cannot find selector ${selector}.`
+    for (const [index, Component] of components.entries()) {
+      const selector = `[data-next-static-index="${index}"]`
+      const root = document.querySelector(selector)
+      if (!root) {
+        throw new Error(
+          `[next-static] Unable to rehydrate static root. Cannot find selector ${selector}.`
+        )
+      }
+      ReactDOM.hydrate(
+        <ApplicationRoot
+          locale={locale}
+          domains={domains}
+          defaultLocale={defaultLocale}
+          locales={locales}
+          basePath={basePath}
+          linkPrefix={linkPrefix}
+          query={query}
+        >
+          <Component {...props} />
+        </ApplicationRoot>,
+        root
       )
     }
-    ReactDOM.hydrate(
-      <ApplicationRoot
-        locale={locale}
-        domains={domains}
-        defaultLocale={defaultLocale}
-        locales={locales}
-        basePath={basePath}
-        linkPrefix={linkPrefix}
-        query={query}
-      >
-        <Component {...props} />
-      </ApplicationRoot>,
-      root
-    )
-  }
-  window.dispatchEvent(new CustomEvent(EV_AFTER_HYDRATION))
-}
-
-;(async () => {
-  try {
-    await init()
+    window.dispatchEvent(new CustomEvent(EV_AFTER_HYDRATION))
   } catch (e) {
     console.error(`[next-static] Error during application init.`, e)
   }
+}
+
+;(() => {
+  if (document.readyState !== 'loading') {
+    return init()
+  }
+  document.addEventListener('DOMContentLoaded', init)
 })()
